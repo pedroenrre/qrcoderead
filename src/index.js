87ac926710cf18f18544e2934b6d2c3171ed2ed9
Pedro Augusto Rodrigues Ferreira <pedro_enrre@hotmail.com>
2019-12-30 13:44:23 -0300

    Customização da status bar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import 'react-native-gesture-handler';
 
 import Routes from './routes';
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar backgroundColor="#008" barStyle="light-content" />
+      <Routes />
+    </>
+  );
 }
 export default App;
