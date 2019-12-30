583c09547cdadee779833455029bbf5c412f3533
Pedro Augusto Rodrigues Ferreira <pedro_enrre@hotmail.com>
2019-12-30 13:45:00 -0300

    Customização do botão voltar da tela Home - Confirmar se deseja fechar o app

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Container, Content} from 'native-base';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {Text, TouchableOpacity, BackHandler, Alert} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from './styles';
 
 // import { Container } from './styles';
 
 export default function Home({navigation}) {
+  useEffect(() => {
+    function exitApp() {
+      Alert.alert(
+        'Sair',
+        'Deseja sair do app?',
+        [
+          {text: 'Não', style: 'cancel'},
+          {text: 'Sim', onPress: () => BackHandler.exitApp()},
+        ],
+        {cancelable: false}
+      );
+      return true;
+    }
+
+    BackHandler.addEventListener('hardwareBackPress', exitApp);
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', exitApp);
+    };
+  }, []);
   return (
     <Container>
       <Content contentContainerStyle={styles.Content}>
